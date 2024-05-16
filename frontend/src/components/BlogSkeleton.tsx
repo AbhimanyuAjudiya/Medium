2a0066b13d949fb1c12a64eb9338795f39a7112f
Abhimanyu Ajudiya <abhimanyuajudiya@gmail.com>
2024-05-16 16:24:39 +0530

    completed frontend and with that completed full project

--- frontend/src/components/BlogSkeleton.tsx
+++ frontend/src/components/BlogSkeleton.tsx
@@ -6,7 +6,7 @@ export const BlogSkeleton = () => {
             <div className="h-4 w-4 bg-gray-200 rounded-full w-48 mb-4"></div>
             <div className="h-2 bg-gray-200 rounded-full mb-2.5"></div>
             <div className="h-2 bg-gray-200 rounded-full mb-2.5"></div>
-            <div className="flex justify-center flex-col pl-2 flex justify-center flex-col">
+            <div className="flex justify-center flex-col pl-2">
             </div>
             <div className="pl-2 font-thin text-slate-500 text-sm flex justify-center flex-col">
                 <div className="h-2 bg-gray-200 rounded-full mb-2.5"></div>
